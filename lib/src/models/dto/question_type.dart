843773efd358a812d0eb5ad81be5b190e4e0bb2a
matt <mkc11267@gmail.com>
2024-07-05 15:29:33 +0800

    Add label to question types

--- lib/src/models/dto/question_type.dart
+++ lib/src/models/dto/question_type.dart
@@ -1,4 +1,14 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 @JsonEnum()
-enum QuestionType { oneAnswer, multipleAnswer, grouping, sequencing, undefined }
+enum QuestionType {
+  oneAnswer('One answer'),
+  multipleAnswer('Multiple answers'),
+  grouping('Grouping'),
+  sequencing('Sequencing'),
+  undefined('Undefined');
+
+  final String label;
+
+  const QuestionType(this.label);
+}
